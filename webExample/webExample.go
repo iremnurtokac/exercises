436ab2d71452b4bbe776ea6a26bec36d5d8303a5
itokac <iremnur.tokac@zalando.de>
2017-02-24 11:08:39 +0100

    Web example 2

--- webExample/webExample.go
+++ webExample/webExample.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "iremnur")
+
+	})
+	http.ListenAndServe(":8080", nil)
+}
